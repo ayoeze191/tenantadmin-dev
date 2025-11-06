738844c79b5adf76643116981d546b500836df45
Olabode Ezekiel <ayoeze191@gmail.com>
2025-11-07 00:24:12 +0100

    finished notification modal

--- src/views/tenants/Tenants.vue
+++ src/views/tenants/Tenants.vue
@@ -4,10 +4,72 @@
       class="rounded-[16px] mt-4 font-inter border-[#36363633] border-[0.75px] border-solid"
     >
       <div class="flex gap-2.5 p-2.5 items-center">
-        <table-header
-          :total-item-count="totalItemCount"
-          title="Tenants"
-        ></table-header>
+        <table-header :total-item-count="totalItemCount" title="Tenants">
+          <div class="flex gap-[10px]">
+            <a-button
+              class="bg-[#000130] text-white flex items-center justify-center py-[12px]"
+            >
+              Add Tenant
+            </a-button>
+            <a-button
+              class="bg-[#FFFFFF] border-[0.75px] border-solid border-[#000130] text-[#000130] flex items-center justify-center py-[12px]"
+            >
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2 4.66406H4"
+                  stroke="black"
+                  stroke-opacity="0.7"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M2 11.3359H6"
+                  stroke="black"
+                  stroke-opacity="0.7"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M12 11.3359H14"
+                  stroke="black"
+                  stroke-opacity="0.7"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M10 4.66406H14"
+                  stroke="black"
+                  stroke-opacity="0.7"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M4 4.66406C4 4.04281 4 3.73218 4.10149 3.48715C4.23682 3.16045 4.49639 2.90088 4.82309 2.76556C5.06812 2.66406 5.37875 2.66406 6 2.66406C6.62125 2.66406 6.93187 2.66406 7.17693 2.76556C7.5036 2.90088 7.7632 3.16045 7.89853 3.48715C8 3.73218 8 4.04281 8 4.66406C8 5.28532 8 5.59594 7.89853 5.84098C7.7632 6.16768 7.5036 6.42724 7.17693 6.56257C6.93187 6.66406 6.62125 6.66406 6 6.66406C5.37875 6.66406 5.06812 6.66406 4.82309 6.56257C4.49639 6.42724 4.23682 6.16768 4.10149 5.84098C4 5.59594 4 5.28532 4 4.66406Z"
+                  stroke="black"
+                  stroke-opacity="0.7"
+                  stroke-width="1.5"
+                />
+                <path
+                  d="M8 11.3359C8 10.7147 8 10.4041 8.10147 10.159C8.2368 9.83234 8.4964 9.57274 8.82307 9.4374C9.06813 9.33594 9.37873 9.33594 10 9.33594C10.6213 9.33594 10.9319 9.33594 11.1769 9.4374C11.5036 9.57274 11.7632 9.83234 11.8985 10.159C12 10.4041 12 10.7147 12 11.3359C12 11.9572 12 12.2678 11.8985 12.5129C11.7632 12.8395 11.5036 13.0991 11.1769 13.2345C10.9319 13.3359 10.6213 13.3359 10 13.3359C9.37873 13.3359 9.06813 13.3359 8.82307 13.2345C8.4964 13.0991 8.2368 12.8395 8.10147 12.5129C8 12.2678 8 11.9572 8 11.3359Z"
+                  stroke="black"
+                  stroke-opacity="0.7"
+                  stroke-width="1.5"
+                />
+              </svg>
+              Filter
+            </a-button>
+          </div>
+        </table-header>
       </div>
 
       <div class="w-full mt-4 h-full">
@@ -107,7 +169,7 @@
           <span class="font-redwing text-2 leading-[100%] font-medium"
             >Tenant information</span
           >
-          <span>
+          <span @click="showModal = false" class="cursor-pointer">
             <svg
               width="14"
               height="14"
@@ -146,9 +208,43 @@
       >
         Send Notification
       </p>
-      <div><BaseInput type="email" placeholder="Email address" /></div>
+      <div>
+        <BaseInput
+          v-model="form.email"
+          type="email"
+          placeholder="Email address"
+        />
+      </div>
       <div class="mt-4">
-        <BaseInput type="textarea" placeholder="Enter Message" :rows="5" />
+        <BaseInput
+          v-model="form.message"
+          type="textarea"
+          placeholder="Enter Message"
+          :rows="5"
+        />
+      </div>
+      <div class="flex flex-wrap gap-2.5 mt-3">
+        <a-button
+          @click="form.message = value"
+          v-for="value in messages"
+          class="py-[12px] flex rounded-[100px] items-center hover:bg-[#F0F0F0] bg-[#FFFFFF] px-[15px] font-inter font-medium text-[12px]"
+        >
+          {{ value }}
+        </a-button>
+      </div>
+      <div
+        class="pt-[16px] border-t-[0.75px] text-[14px] flex gap-[10px] mt-[16px]"
+      >
+        <a-button
+          class="w-full py-[8px] flex items-center justify-center bg-[#000130] text-white rounded-[8px]"
+        >
+          Send to Unit
+        </a-button>
+        <a-button
+          class="w-full py-[8px] flex items-center justify-center text-black rounded-[8px]"
+        >
+          Send to Tenant
+        </a-button>
       </div>
     </a-modal>
   </div>
@@ -166,6 +262,7 @@ import TableHeader from "@/components/TableHeader.vue";
 import BasePagination from "@/components/BasePagination.vue";
 import BaseInput from "@/components/BaseInput.vue";
 import { h } from "vue";
+import { message } from "ant-design-vue";
 export default {
   components: {
     "table-component": V2Table,
@@ -179,6 +276,14 @@ export default {
   },
   data() {
     return {
+      messages: [
+        "Your lease Would Expire Soon",
+        "A noise complain has b...",
+        "Your service request has been sorted",
+        "Your rent is still unpaid",
+        "Kindly pay your rent please",
+      ],
+      form: { email: "", message: "" },
       showModal: false,
       totalItemCount: 0,
       currentPage: 1,
